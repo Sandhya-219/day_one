1c4c25a9f5a3d6ace56388af0911f33cb175258e
Sandhya-219 <sandhyalagishetti1907@gmail.com>
2026-03-23 17:39:55 +0530

    latest

--- obj/Release/net10.0/project1.AssemblyInfo.cs
+++ obj/Release/net10.0/project1.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("project1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c98e6e95db3943f8a8d53cddea7bddf7a03d6c19")]
 [assembly: System.Reflection.AssemblyProductAttribute("project1")]
